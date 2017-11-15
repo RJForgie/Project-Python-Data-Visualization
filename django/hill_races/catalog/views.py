efd3cc4e47a2a4e374a55fadfdc8b1f22dbb0f5c
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-11-15 11:55:21 +0000

    add scatter graph

--- django/hill_races/catalog/views.py
+++ django/hill_races/catalog/views.py
@@ -24,15 +24,22 @@ def data(request):
     # Render the HTML template index.html with the data in the context variable
     all_races=Race.objects.all()
     months = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    distance_climb = []
     for index, race in enumerate(all_races):
         if (int(race.date[6:8]) == 17):
             month = int(race.date[3:5])
             months[month - 1] += 1
 
+    for index, race in enumerate(all_races):
+        if (int(race.distance) < 90.0):
+            single_race = []
+            single_race.append(race.climb)
+            single_race.append(race.distance)
+            distance_climb.append(single_race)
     return render(
         request,
         'data_dashboard.html',
-        context={'months':months},
+        context={'months':months, 'distance_climb':distance_climb},
     )
 
 class RaceListView(generic.ListView):
